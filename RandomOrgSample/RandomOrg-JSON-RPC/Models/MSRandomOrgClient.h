3c3a0cf049a8e42107a6037d29f36397a7e81107
Maksym Savisko <stowyn@gmail.com>
2017-08-27 14:54:26 +0300

    [Documentation] Update basic documentation.

--- RandomOrgSample/RandomOrg-JSON-RPC/Models/MSRandomOrgClient.h
+++ RandomOrgSample/RandomOrg-JSON-RPC/Models/MSRandomOrgClient.h
@@ -10,20 +10,63 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Completion handler block for generations
+
+ result: Array of number that was generated
+ error: Error with info from random.org service
+ */
 typedef void (^ _Nullable MSRandomOrgClientHandlerBlock)(NSArray <NSNumber *> * _Nullable result ,  NSError * _Nullable error);
 
 @interface MSRandomOrgClient : NSObject
 
+/**
+ Random Org API Key.
+ Look: https://api.random.org/api-keys/beta
+ */
 @property (nonatomic, strong, readonly, nullable) NSString *apiKey;
 
+/**
+ The dispatch queue for `resultHandlerBlock`. If `NULL` (default), the main queue is used.
+ */
 @property (nonatomic, strong, nullable) dispatch_queue_t callBackQueue;
 
+/**
+ Create new instance of random org client
+
+ @param apiKey from Random.org service. Look: https://api.random.org/api-keys/beta
+ @return new instance
+ */
 + (instancetype) clientWithApiKey:(NSString *) apiKey;
 
+/**
+ Initialize new random org client with main thread callback queue.
+
+ @param apiKey from Random.org service. Look: https://api.random.org/api-keys/beta
+ @return new instance
+ */
 - (instancetype) initWithApiKey:(NSString *) apiKey;
 
-- (instancetype) initWithApiKey:(NSString *) apiKey andCallBackQueue:(nullable dispatch_queue_t) callBackQueue;
+/**
+ Initalize new random org client
 
+ @param apiKey from Random.org service. Look: https://api.random.org/api-keys/beta
+ @param callBackQueue where resultHandlerBlock will be invoked
+ @return new instance
+ */
+- (instancetype) initWithApiKey:(NSString *) apiKey
+               andCallBackQueue:(nullable dispatch_queue_t) callBackQueue;
+
+/**
+ Generate Integers via Random.org service
+
+ @param minValue Minimum integer value that may exist in response of resulted integers.
+ @param maxValue Maximum integer value that may exist in response of resulted integers.
+ @param numberValue Number of integers, that may be generated
+ @param unigueFlag YES - integers in response will be unique. NO - could be repeatable.
+ @param baseValue Indicate, what kind of system will be used for response. 2 - binary, 10 - Decimal, etc.
+ @param resultHandlerBlock A block object to be executed when the generations finishes. This block has no return value and takes three arguments: result of generations (could be nullable) and the error that occurred, if any problems exist.
+ */
 - (void) generateIntegersMin:(NSInteger)minValue
                          max:(NSInteger)maxValue
                       number:(NSUInteger)numberValue
@@ -32,6 +75,14 @@ typedef void (^ _Nullable MSRandomOrgClientHandlerBlock)(NSArray <NSNumber *> * 
                resultHandler:(MSRandomOrgClientHandlerBlock) resultHandlerBlock;
 
 
+/**
+ Generate Integers via Random.org service
+
+ @param minValue Minimum integer value that may exist in response of resulted integers.
+ @param maxValue Maximum integer value that may exist in response of resulted integers.
+ @param numberValue Number of integers, that may be generated
+ @param resultHandlerBlock A block object to be executed when the generations finishes. This block has no return value and takes three arguments: result of generations (could be nullable) and the error that occurred, if any problems exist.
+ */
 - (void) generateIntegersMin:(NSInteger)minValue
                          max:(NSInteger)maxValue
                       number:(NSUInteger)numberValue
@@ -41,16 +92,40 @@ typedef void (^ _Nullable MSRandomOrgClientHandlerBlock)(NSArray <NSNumber *> * 
 
 @interface MSRandomOrgClient (Global)
 
+/**
+ Returns the "global" random org client that represents the currently setup client.
+ 
+ The `currentClient` is a convenient representation of the client of the
+ current client and is used by other SDK components.
+ */
 + (nullable MSRandomOrgClient *) currentClient;
 
+/**
+ Sets the "global" random org client that represents the currently setup client.
+
+ @param currentCLient The random org client to set.
+ */
 + (void) setCurrentClient:(nullable MSRandomOrgClient *) currentCLient;
 
+/**
+ Sets the "global" random org client that represents the currently setup client.
+
+ @param apiKey from Random.org service. Look: https://api.random.org/api-keys/beta
+ */
 + (void) setupWithApiKey:(NSString *) apiKey;
 
 @end
 
 @interface MSRandomOrgClient (Methods)
 
+/**
+ Generate Integers via Random.org service using 'global' random org client.
+
+ @param minValue Minimum integer value that may exist in response of resulted integers.
+ @param maxValue Maximum integer value that may exist in response of resulted integers.
+ @param numberValue Number of integers, that may be generated
+ @param resultHandlerBlock A block object to be executed when the generations finishes. This block has no return value and takes three arguments: result of generations (could be nullable) and the error that occurred, if any problems exist.
+ */
 + (void) generateIntegersMin:(NSInteger)minValue
                          max:(NSInteger)maxValue
                       number:(NSUInteger)numberValue
--- RandomOrgSample/RandomOrg-JSON-RPC/RequestManager/MSRequestResponse.h
+++ RandomOrgSample/RandomOrg-JSON-RPC/RequestManager/MSRequestResponse.h
@@ -10,13 +10,42 @@
 
 @interface MSRequestResponse : NSObject
 
-@property BOOL isSuccess;
-@property (nonatomic, strong) NSDate *requestStartTime;
-@property (nonatomic, strong) id object;
-@property (nonatomic, strong) NSError *error;
-@property (nonatomic, strong) NSString *contentId;
+/**
+ Indicate, that response was successed (internet OK, response from server OK).
+ */
+@property (nonatomic) BOOL isSuccess;
+
+/**
+ Local device time, that indicate when reuqest started.
+ */
+@property (nonatomic, strong, nonnull, readonly) NSDate *requestStartTime;
+
+/**
+ Response object (usually JSON)
+ */
+@property (nonatomic, strong, nullable) id object;
+
+/**
+ Error from service of requesting proccess (if exist).
+ */
+@property (nonatomic, strong, nullable) NSError *error;
+
+/**
+ Content ID for request. Usually it is an integer from 0 to 32768
+ */
+@property (nonatomic, strong, nullable) NSString *contentId;
+
+/**
+ HTTP status response code.
+ If it could not be parsed, return 0.
+ */
 @property (nonatomic) NSInteger statusCode;
 
-+ (instancetype) response;
+/**
+ Create new instance of request
+
+ @return new instance of empty response
+ */
++ (nonnull instancetype) response;
 
 @end
